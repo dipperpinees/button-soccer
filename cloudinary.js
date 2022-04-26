254bc17440f84845dea570580f6cc4574c503280
Hiep Nguyen <79491170+hiepnguyen223@users.noreply.github.com>
2022-04-26 21:07:27 +0700

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary =  require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 const cloundaryConfig = cloudinary.config({
-    cloud_name: process.env.cloudname,
-    api_key: process.env.apikey,
-    api_secret: process.env.apisecret,
+    cloud_name: process.env.CLOUDNAME,
+    api_key: process.env.APIKEY,
+    api_secret: process.env.APISECRET,
 });
 
 const storage = new CloudinaryStorage({
